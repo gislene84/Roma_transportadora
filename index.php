bcc05aec8892bfcaba1ce97e6e632d5b488b80b7
gislene84 <50671914+gislene84@users.noreply.github.com>
2021-08-25 18:49:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Roma Transportes</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <section class="topo">
+        <div class="center">
+            <header>
+                <div class="logo"><img src="img/logo1.jpg"/></div><!--logo-->
+            </header>
+            <ul class="menu-desktop">
+                <li><a target="_blank" href="sobre.php">NOSSA HISTÓRIA</a></li>
+                <li><a href="#">NOSSO BLOG</a></li>
+                <li><a href="#">CONTATOS</a></li>
+            </ul><!--menu-desktop-->
+            <div class="clear"></div><!--clear-->
+        </div><!--center-->
+    </section><!--topo-->
+    <section class="hero">
+            <div class="container-menu">
+                <button><a target="_blank" href="painel.php">MEU PAINEL</a></button>
+                <button><a target="_blank" href="carga.php">RASTREAR CARGAS</a></button>
+            </div><!--container-menu-->
+        </section><!--hero-->
+        <section class="hero-1">
+            <div class="container">
+                <h1>Calcule e contrate seu frete, totalmente online!</h1>
+                <h2>Temos o melhor preço, praticidade e comodidade para enviar suas mercadorias de forma simples e rápida.</h2>
+                <div class="imagem-principal"></div><!--imagem-principal-->
+                <button><a href="sobre.html"></a>COTE AGORA</button>
+            </div>
+        </section><!--hero-1-->
+</body>
+</html>
